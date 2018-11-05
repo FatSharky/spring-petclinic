21e469e2bc54bebaebdff6a19e5fc8a0760df73b
Uladzislau Hapeyenka <uladzislau_hapeyenka@epam.com>
2018-11-05 15:34:35 +0300

    Abstract

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -30,7 +30,7 @@ import javax.persistence.MappedSuperclass;
  * @author Juergen Hoeller
  */
 @MappedSuperclass
-public class BaseEntity implements Serializable {
+public abstract class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
